3177f2de4cf2d1f48d533fa43036ef406a299918
Edgar Martinez <eddmtz96@gmail.com>
2024-08-28 19:05:54 -0600

    move install_fonts script to setup script

--- install_fonts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-mkdir JetBrainsMono
-tar -xf JetBrainsMono.tar.xz -C JetBrainsMono
-mkdir -p ~/.local/share/fonts/JetBrainsMono
-mv JetBrainsMono/JetBrainsMono*.ttf ~/.local/share/fonts/JetBrainsMono/
-fc-cache -v
-rm -r JetBrainsMono*
--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# ---------------- Fonts ----------------
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+mkdir JetBrainsMono
+tar -xf JetBrainsMono.tar.xz -C JetBrainsMono
+mkdir -p ~/.local/share/fonts/JetBrainsMono
+mv JetBrainsMono/JetBrainsMono*.ttf ~/.local/share/fonts/JetBrainsMono/
+fc-cache -v > /dev/null 2>&1
+rm -r JetBrainsMono*
+
 # ---------------- Vim editor ----------------
 vim_directory=$PWD/vim
 
