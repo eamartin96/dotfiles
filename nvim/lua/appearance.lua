f88fdb709591c630e67ebbd0fc4a3a7d05e16268
Edgar Martinez <eddmtz96@gmail.com>
2025-02-27 16:05:55 -0600

    set number line to transparent

--- nvim/lua/appearance.lua
+++ nvim/lua/appearance.lua
@@ -8,4 +8,10 @@ local api = vim.api
 cmd([[ colorscheme onedark ]])
 
 -- transparent background
-cmd([[ hi normal guibg=NONE ]])
+cmd([[ hi Normal guibg=NONE ctermbg=NONE ]])
+
+-- make LineNr (line numbers) transparent
+cmd('highlight LineNr ctermbg=NONE guibg=NONE')
+
+-- make CursorLineNr (current line number) transparent
+cmd('highlight CursorLineNr ctermbg=NONE guibg=NONE')
